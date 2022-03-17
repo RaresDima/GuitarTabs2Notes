c4c46f1fe18cde85fc898e4b03c1a8082fd00891
Rares Dima <rares.dima96@yahoo.com>
2022-03-17 12:56:33 +0200

    Adjusted README notes.

--- pyinstaller_build.bat
+++ pyinstaller_build.bat
@@ -1,6 +1,7 @@
 cls
 del /F /Q ".\dist"
 pyinstaller ^
+  --clean ^
   --noconfirm ^
   --name "GuitarTabs2Notes" ^
   --add-data ".\data\config.yaml;.\data" ^
@@ -8,8 +9,9 @@ pyinstaller ^
   --add-data ".\data\img\bass_clef.svg;.\data\img" ^
   --add-binary ".\data\font\BeautifulPeople.ttf;.\data\font" ^
   --add-binary ".\data\font\Countryside.ttf;.\data\font" ^
+  --add-data ".\README.MD;." ^
   ".\main.py"
 del /F /Q ".\GuitarTabs2Notes.spec"
 del /F /Q ".\build"
 
-.\dist\GuitarTabs2Notes\GuitarTabs2Notes.exe
+:: .\dist\GuitarTabs2Notes\GuitarTabs2Notes.exe
